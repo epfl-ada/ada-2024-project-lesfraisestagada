d8208c10e4944351e64417a10863a79515cabb41
Bryan Gotti <bry.gotti@outlook.com>
2024-11-15 17:11:48 +0100

    Last clean up

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,31 +1,7 @@
 import torch
 import numpy as np
-from torch.utils.data import DataLoader, Dataset
 import pandas as pd
 import os
-
-
-
-class SomeDataset(Dataset):
-    """
-    A dataset implements 2 functions
-        - __len__  (returns the number of samples in our dataset)
-        - __getitem__ (returns a sample from the dataset at the given index idx)
-    """
-
-    def __init__(self, dataset_parameters, **kwargs):
-        super().__init__()
-        ...
-
-
-class SomeDatamodule(DataLoader):
-    """
-    Allows you to sample train/val/test data, to later do training with models.
-        
-    """
-    def __init__(self):
-        super().__init__()
-        ...
         
 
 def load_articles_dataframe(path = "./data/wikispeedia_paths-and-graph/"):
